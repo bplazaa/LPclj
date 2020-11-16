f83785de42f34cd04208ddca6a592d50105b4da9
Bryan Plaza <bryanplaza_@hotmail.com>
2020-11-15 19:05:45 -0600

    Lexico y ejemplos en archivo

---
+++ lexico.py
@@ -0,0 +1,91 @@
+ # ------------------------------------------------------------
+ # Grupo Clojure 1
+ # ------------------------------------------------------------
+
+import ply.lex as lex
+
+reserved = {
+    'if' : 'IF',
+    'when' : 'THEN',
+    'for' : 'FOR',
+    'def':'DEF',
+    'and' : 'AND',
+    'not' : 'NOT',
+    'true' : 'TRUE',
+    'false' : 'FALSE'
+}
+
+tokens = (
+    'NUMBER',
+    'STRING',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+    'LCOR',
+    'RCOR',
+    'MAYORQUE',
+    'MENORQUE',
+    'IGUAL',
+    'ID'
+ ) + tuple(reserved.values())
+
+
+t_MINUS   = r'-'
+t_PLUS    = r'\+'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_LCOR  = r'\['
+t_RCOR  = r'\]'
+t_MAYORQUE = r'>'
+t_MENORQUE = r'<'
+t_IGUAL = r'='
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')    
+    return t
+
+def t_STRING(t):
+    r'^"[a-zA-Z_0-9\s]*"$'
+    t.value = str(t.value)    
+    return t
+
+t_ignore  = ' \t'
+ 
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+ 
+
+archivo = open("codigo.txt", 'r',  encoding="utf-8")
+
+contenido = archivo.read()
+
+archivo.close()
+
+ # Give the lexer some input
+lexer.input(contenido)
+
+ # Tokenize
+while True:
+    tok = lexer.token()
+    if not tok: 
+        break      # No more input
+    print(tok)
